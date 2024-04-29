48b53ac1ea822022181f482d2059150781deef88
Wesley Zhuang <zaganon@gmail.com>
2024-04-29 15:08:13 -0700

    Steep Parallax Mapping

--- shaders/include/parallax.glsl
+++ shaders/include/parallax.glsl
@@ -2,14 +2,32 @@ vec2 parallaxMapping(in vec2 texCoord, in vec3 viewDir, in sampler2D depthMap, i
     #if PARALLAX == 0
     return texCoord;
 
-    #elif PARALLAX == 1
+    #elif PARALLAX == 1 //Simple Parallax Mapping
     float height = 1.0 - texture(depthMap, texCoord).a;
     if (height < 0.0001){
         return texCoord;
     }
-    vec2 p = viewDir.xy / viewDir.z * (height * max(textureSize.x, textureSize.y) * 0.25);
+    vec2 p = viewDir.xy / viewDir.z * (height * max(textureSize.x, textureSize.y) * PARALLAX_DEPTH);
     return textureBounds.st + mod(texCoord - p - textureBounds.st, textureSize);
-    #elif PARALLAX == 2
+
+    #elif PARALLAX == 2 //Steep Parallax Mapping
+    vec2 currentTexCoords = texCoord;
+    float currentDepthValue = 1.0 - texture(depthMap, currentTexCoords).a;
+    if (currentDepthValue < 0.0001){
+        return texCoord;
+    }
+    float layerDepth = 1.0 / PARALLAX_LAYERS;
+    float currentDepthLayer = 0.0;
+    vec2 deltaTexCoords = ((viewDir.xy / viewDir.z) * PARALLAX_DEPTH) / PARALLAX_LAYERS;
     
+    while(currentDepthLayer < currentDepthValue){
+        // This line is bad performance. Consider using denormalized atlas coordinates instead.
+        currentTexCoords = textureBounds.st + mod(currentTexCoords - deltaTexCoords - textureBounds.st, textureSize);
+        currentDepthValue = 1.0 - texture(depthMap, currentTexCoords).a;
+        currentDepthLayer += layerDepth;
+    }
+    return currentTexCoords;
+
+    #elif PARALLAX == 3 //Parallax Occlusion Mapping
     #endif
 }
